05197ac53b9cc605026574406142682e6db35db7
Dmitriy Myagkikh <myagkikh23121987@gmail.com>
2018-06-02 13:04:49 +0300

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,16 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	int [][] namber = new int[3][4];
-	for( int i = 0; i < 0; i++){
-	for ( )
-    }
+	public static void main(String[] args) {
+		int[][] namber = new int[3][4];
+		int namber2 = 1;
+		for (int i = 0; i < 3; i++) {
+			for (int b = 0; b < 4; b++) {
+				namber[i][b] = namber2 ++;
+
+				System.out.print(namber[i][b] + " ");
+			}
+			System.out.println();
+		}
+	}
 }
-[jhjhgkjl;]
